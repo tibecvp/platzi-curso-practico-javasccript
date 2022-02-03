6ca1fe23326909b2ab6d877d7727fe74e183a1cd
Tibe Calderon <tibecvp@gmail.com>
2022-02-02 22:41:02 -0500

    Se completa el taller 2 y se avanza en el promedio y la media del taller 3

---
+++ descuentos.js
@@ -0,0 +1,28 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = (priceValue * (100 - discountValue)) / 100;
+    const resultPrice = document.getElementById("ResultPrice");
+    resultPrice.innerText = "El precio con descuento es: $" + precioConDescuento + " dolaretes";
+    resultPrice.style.display = "block";
+}
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
