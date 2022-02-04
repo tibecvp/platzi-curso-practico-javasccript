cb8770156ee4df96ffb790bc6f2883e66dc9bc95
Tibe Calderon <tibecvp@gmail.com>
2022-02-03 23:46:07 -0500

    Se completa la lógica de los 4 talleres en los archivos js y se agrega navbar en los html

---
+++ salarios.js
@@ -0,0 +1,101 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000
+});
+
+colombia.push({
+    name: "Natalia",
+    salary: 1500
+});
+
+colombia.push({
+    name: "Andrés",
+    salary: 4000
+});
+
+colombia.push({
+    name: "Dayana",
+    salary: 3500
+});
+
+colombia.push({
+    name: "Gustavo",
+    salary: 3500
+});
+
+colombia.push({
+    name: "Lucho",
+    salary: 4000
+});
+
+colombia.push({
+    name: "Candy",
+    salary: 1800
+});
+
+colombia.push({
+    name: "Jair",
+    salary: 14000
+});
+
+colombia.push({
+    name: "Daniel",
+    salary: 5000
+});
+
+colombia.push({
+    name: "Wilfer",
+    salary: 6000
+});
+
+colombia.push({
+    name: "Ferney",
+    salary: 3000
+});
+
+colombia.push({
+    name: "Mauricio",
+    salary: 3000
+});
+
+colombia.push({
+    name: "Sergio",
+    salary: 2400
+});
+
+colombia.push({
+    name: "Santiago",
+    salary: 10000
+});
+
+colombia.push({
+    name: "Ronald",
+    salary: 1000
+});
+
+colombia.push({
+    name: "Angelica",
+    salary: 4500
+});
+
+colombia.push({
+    name: "Julian",
+    salary: 1400
+});
+
+colombia.push({
+    name: "Janisse",
+    salary: 5000
+});
+
+colombia.push({
+    name: "Catalina",
+    salary: 7000
+});
+
+colombia.push({
+    name: "Keilly",
+    salary: 7000
+});
